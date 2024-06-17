07fe23dfee1a8a16b5ca91f4791dd977dbae9035
insung0927 <78968690+Fiddich-Dev@users.noreply.github.com>
2024-06-18 00:56:28 +0900

    프로젝트 생성

---
+++ CICDTest/CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by 황인성 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
